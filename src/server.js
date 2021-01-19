1c087e23b8a445b95d607774991e9d7e6b1c7a30
PanicP <filmous2136@gmail.com>
2021-01-19 18:16:53 +0700

    WIP: Add necessary tools and add some structure

--- src/server.js
+++ src/server.js
@@ -10,20 +10,19 @@ app.use(cors())
 app.get('/api/cards', (req, res) => {
   const { name, type, limit = 20 } = req.query
 
-  if (_.every([name, type], item => item === undefined)) {
+  if (_.every([name, type], (item) => item === undefined)) {
     return res.json({ cards: cards.slice(0, limit) })
   }
 
   res.json({
-    cards: _.filter(cards, card => {
+    cards: _.filter(cards, (card) => {
       const name = _.toUpper(_.get(req, 'query.name', ''))
       const type = _.toUpper(_.get(req, 'query.type', ''))
       const checkName = _.includes(_.toUpper(card.name), name)
       const checkType = _.includes(_.toUpper(card.type), type)
       return checkName && checkType
-    })
+    }),
   })
-
 })
 
 app.listen(3030, () => console.log('app start @ port 3030'))
