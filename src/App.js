8a872dedd3fe09a374b216736d5f62242c412528
PanicP <filmous2136@gmail.com>
2021-01-19 17:51:26 +0700

    DONE: Init Project and add some structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+const COLORS = {
+  Psychic: "#f8a5c2",
+  Fighting: "#f0932b",
+  Fairy: "#c44569",
+  Normal: "#f6e58d",
+  Grass: "#badc58",
+  Metal: "#95afc0",
+  Water: "#3dc1d3",
+  Lightning: "#f9ca24",
+  Darkness: "#574b90",
+  Colorless: "#FFF",
+  Fire: "#eb4d4b",
+};
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>test render</div>
     </div>
   );
-}
+};
 
 export default App;
